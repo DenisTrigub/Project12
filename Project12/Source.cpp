26b18960a155e2a570605e268264f605d2ab39fc
User <User@DESKTOP-802SV85>
2022-04-18 19:50:35 +0300

    task 2 done

--- Project12/Source.cpp
+++ Project12/Source.cpp
@@ -3,5 +3,22 @@ using namespace std;
 
 int main()
 {
+    const unsigned short  size = 10;
+    int arr[size] = { 1,2,3,4,5,6,7,8,9,10 };
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    int* parr = arr;
+    int n = 0;
+    int* ptr;
 
+    ptr = &n;
+    *ptr = 5;
+
+    for (int i = size - 1; i >= 0; i--) {
+        cout << parr[i] << " ";
+    }
+    return 0;
+   
 }
