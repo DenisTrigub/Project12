71549d521efb8cb5d3f876cbce7507da50f683e3
User <User@DESKTOP-802SV85>
2022-04-18 19:55:06 +0300

    task 3 done

--- Project12/Source.cpp
+++ Project12/Source.cpp
@@ -3,5 +3,24 @@ using namespace std;
 
 int main()
 {
+    srand(time(0));
+    const unsigned short  size = 10;
+    int arr[size];
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 100;
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    int* parr = arr;
+    int n = 0;
+    int* ptr;
+
+    ptr = &n;
+    *ptr = 5;
+
+    for (int i = size - 1; i >= 0; i--) {
+        cout << parr[i] << " ";
+    }
+    return 0;
 
 }
